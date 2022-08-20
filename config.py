2b2380f7c46f520adc94e52d5068aa4164b40292
belousovsergey56 <belousovsergej56@gmail.com>
2022-08-20 14:23:52 +0300

    add flask_login ang int this

--- config.py
+++ config.py
@@ -2,7 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import os
-import _mysql_connector
+# import _mysql_connector
+from flask_login import LoginManager
 
 load_dotenv()
 db_password = os.getenv('mysql_password_root')
@@ -17,6 +18,9 @@ app.config['UPLOADED_IMAGES_DEST'] = 'static/images'
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///repinart.db"
 # app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+mysqlconnector://root:{db_password}@localhost/repin_art"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
 db = SQLAlchemy(app)
